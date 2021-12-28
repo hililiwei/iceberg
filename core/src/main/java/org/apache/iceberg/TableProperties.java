05370507983764878164bd2d4b0dd78044d371e2
liliwei <hililiwei@gmail.com>
2021-12-28 09:15:30 +0800

    ORC: Add ORC support for write properties

--- core/src/main/java/org/apache/iceberg/TableProperties.java
+++ core/src/main/java/org/apache/iceberg/TableProperties.java
@@ -112,10 +112,10 @@ public class TableProperties {
   public static final String AVRO_COMPRESSION_DEFAULT = "gzip";
 
   public static final String ORC_STRIPE_SIZE_BYTES = "write.orc.stripe-size-bytes";
-  public static final String ORC_STRIPE_SIZE_BYTES_DEFAULT = "67108864"; // 64L * 1024 * 1024
+  public static final long ORC_STRIPE_SIZE_BYTES_DEFAULT = 64L * 1024 * 1024; // 64 MB
 
   public static final String ORC_BLOCK_SIZE_BYTES = "write.orc.block-size-bytes";
-  public static final String ORC_BLOCK_SIZE_BYTES_DEFAULT = "268435456"; // 256L * 1024 * 1024
+  public static final long ORC_BLOCK_SIZE_BYTES_DEFAULT = 256L * 1024 * 1024; // 256 MB
 
   public static final String SPLIT_SIZE = "read.split.target-size";
   public static final long SPLIT_SIZE_DEFAULT = 128 * 1024 * 1024; // 128 MB

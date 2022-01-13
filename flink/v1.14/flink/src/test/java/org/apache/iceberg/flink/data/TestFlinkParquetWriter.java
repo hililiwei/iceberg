ff5e9dccfd5c6764f2d0f4f68c5421edc1121f5c
liliwei <hililiwei@gmail.com>
2022-01-13 22:05:52 +0800

    Parquet: unit test for parquet table properties

--- flink/v1.14/flink/src/test/java/org/apache/iceberg/flink/data/TestFlinkParquetWriter.java
+++ flink/v1.14/flink/src/test/java/org/apache/iceberg/flink/data/TestFlinkParquetWriter.java
@@ -21,22 +21,53 @@ package org.apache.iceberg.flink.data;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
+import java.util.UUID;
 import org.apache.flink.table.data.RowData;
 import org.apache.flink.table.types.logical.LogicalType;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.iceberg.DataFile;
+import org.apache.iceberg.DeleteFile;
+import org.apache.iceberg.FileFormat;
 import org.apache.iceberg.Files;
+import org.apache.iceberg.FindFiles;
+import org.apache.iceberg.RowDelta;
 import org.apache.iceberg.Schema;
+import org.apache.iceberg.SerializableTable;
+import org.apache.iceberg.Table;
+import org.apache.iceberg.TableProperties;
 import org.apache.iceberg.data.DataTest;
 import org.apache.iceberg.data.RandomGenericData;
 import org.apache.iceberg.data.Record;
 import org.apache.iceberg.data.parquet.GenericParquetReaders;
 import org.apache.iceberg.flink.FlinkSchemaUtil;
+import org.apache.iceberg.flink.SimpleDataUtil;
 import org.apache.iceberg.flink.TestHelpers;
+import org.apache.iceberg.flink.sink.RowDataTaskWriterFactory;
+import org.apache.iceberg.flink.sink.TaskWriterFactory;
 import org.apache.iceberg.io.CloseableIterable;
+import org.apache.iceberg.io.CloseableIterator;
 import org.apache.iceberg.io.FileAppender;
+import org.apache.iceberg.io.TaskWriter;
+import org.apache.iceberg.io.WriteResult;
 import org.apache.iceberg.parquet.Parquet;
+import org.apache.iceberg.relocated.com.google.common.collect.ImmutableList;
+import org.apache.iceberg.relocated.com.google.common.collect.ImmutableMap;
+import org.apache.iceberg.relocated.com.google.common.collect.Lists;
+import org.apache.parquet.column.ColumnDescriptor;
+import org.apache.parquet.column.page.DataPageV1;
+import org.apache.parquet.column.page.DictionaryPage;
+import org.apache.parquet.column.page.PageReadStore;
+import org.apache.parquet.hadoop.ParquetFileReader;
+import org.apache.parquet.hadoop.metadata.ParquetMetadata;
+import org.apache.parquet.schema.MessageType;
+import org.apache.parquet.schema.MessageTypeParser;
 import org.junit.Assert;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
 public class TestFlinkParquetWriter extends DataTest {
@@ -85,5 +116,142 @@ public class TestFlinkParquetWriter extends DataTest {
     writeAndValidate(RandomRowData.convert(schema,
         RandomGenericData.generateFallbackRecords(schema, NUM_RECORDS, 21124, NUM_RECORDS / 20)),
         schema);
+  }
+
+  @Test
+  public void testParquetProperties() throws Exception {
+    final MessageType schemaSimple =
+        MessageTypeParser.parseMessageType(
+            "message m {" +
+                "  optional int32 id = 1;" +
+                "  optional binary data (STRING) = 2;" +
+                "}");
+
+    final ColumnDescriptor colADesc = schemaSimple.getColumns().get(0);
+    final ColumnDescriptor colBDesc = schemaSimple.getColumns().get(1);
+
+    List<ColumnDescriptor> columnDescriptors = Arrays.asList(colADesc, colBDesc);
+
+    int expectedRowCount = 100000;
+    List<RowData> rows = Lists.newArrayListWithCapacity(expectedRowCount);
+    for (int i = 0; i < expectedRowCount; i++) {
+      rows.add(SimpleDataUtil.createRowData(1, UUID.randomUUID().toString().substring(0, 10)));
+    }
+
+    String location = temp.getRoot().getAbsolutePath();
+
+    ImmutableMap<String, String> properties = ImmutableMap.of(
+        TableProperties.PARQUET_ROW_GROUP_SIZE_BYTES, String.valueOf(expectedRowCount * 10),
+        TableProperties.PARQUET_PAGE_SIZE_BYTES, String.valueOf(expectedRowCount),
+        TableProperties.PARQUET_DICT_SIZE_BYTES, String.valueOf(expectedRowCount),
+        TableProperties.PARQUET_COMPRESSION, "uncompressed");
+
+    Table table = SimpleDataUtil.createTable(location, properties, false);
+
+    writeAndCommit(table, ImmutableList.of(), false, rows);
+    table.refresh();
+
+    CloseableIterator<DataFile> iterator =
+        FindFiles.in(table).collect().iterator();
+
+    Assert.assertTrue(iterator.hasNext());
+
+    DataFile dataFile = iterator.next();
+    Path path = new Path((String) dataFile.path());
+
+    Configuration configuration = new Configuration();
+    ParquetMetadata footer = ParquetFileReader.readFooter(configuration, path);
+    ParquetFileReader parquetFileReader = new ParquetFileReader(
+        configuration, footer.getFileMetaData(), path, footer.getBlocks(), columnDescriptors);
+    Assert.assertEquals(2, footer.getBlocks().size());
+
+    int colAPageANum = 0;
+    int colBPageNum = 0;
+    long rowCount = 0;
+    PageReadStore pageReadStore;
+    while ((pageReadStore = parquetFileReader.readNextRowGroup()) != null) {
+
+      rowCount += pageReadStore.getRowCount();
+
+      DictionaryPage colADictionaryPage = readDictionaryPage(colADesc, pageReadStore);
+      String colAEncodingName = colADictionaryPage.getEncoding().name();
+      Assert.assertEquals("PLAIN_DICTIONARY", colAEncodingName);
+      Assert.assertTrue(
+          "The size of dictionary page should be smaller than " + expectedRowCount,
+          colADictionaryPage.getUncompressedSize() <= expectedRowCount);
+
+      DictionaryPage colBDictionaryPage = readDictionaryPage(colADesc, pageReadStore);
+      String colBEncodingName = colBDictionaryPage.getEncoding().name();
+      Assert.assertEquals("PLAIN_DICTIONARY", colBEncodingName);
+      Assert.assertTrue(
+          "The size of dictionary page should be smaller than " + expectedRowCount,
+          colBDictionaryPage.getUncompressedSize() <= expectedRowCount);
+
+      DataPageV1 colADataPage;
+      while ((colADataPage = readNextPage(colADesc, pageReadStore)) != null) {
+        Assert.assertTrue(
+            "The size of each page should be smaller than " + expectedRowCount,
+            colADataPage.getUncompressedSize() <= expectedRowCount);
+        colAPageANum++;
+      }
+      DataPageV1 colBDataPage;
+      while ((colBDataPage = readNextPage(colBDesc, pageReadStore)) != null) {
+        Assert.assertTrue(
+            "The size of each page should be smaller than " + expectedRowCount,
+            colBDataPage.getUncompressedSize() <= expectedRowCount);
+        colBPageNum++;
+      }
+    }
+
+    Assert.assertEquals("should have 6 pages.", 6, colAPageANum);
+    Assert.assertEquals("should have 16 pages.", 16, colBPageNum);
+    Assert.assertEquals(expectedRowCount, rowCount);
+  }
+
+  private void writeAndCommit(Table table, List<Integer> eqFieldIds, boolean upsert, List<RowData> rows)
+      throws IOException {
+    TaskWriter<RowData> writer = createTaskWriter(table);
+    for (RowData row : rows) {
+      writer.write(row);
+    }
+    RowDelta delta = table.newRowDelta();
+    WriteResult result = writer.complete();
+
+    for (DataFile dataFile : result.dataFiles()) {
+      delta.addRows(dataFile);
+    }
+
+    for (DeleteFile deleteFile : result.deleteFiles()) {
+      delta.addDeletes(deleteFile);
+    }
+
+    delta.commit();
+  }
+
+  /**
+   * Read the next page for a column
+   */
+  private DataPageV1 readNextPage(ColumnDescriptor colDesc, PageReadStore pageReadStore) {
+    return (DataPageV1) pageReadStore.getPageReader(colDesc).readPage();
+  }
+
+  /**
+   * Read the next page for a column
+   */
+  private DictionaryPage readDictionaryPage(ColumnDescriptor colDesc, PageReadStore pageReadStore) {
+    return pageReadStore.getPageReader(colDesc).readDictionaryPage();
+  }
+
+  private TaskWriter<RowData> createTaskWriter(Table table) {
+    TaskWriterFactory<RowData> taskWriterFactory = new RowDataTaskWriterFactory(
+        SerializableTable.copyOf(table),
+        SimpleDataUtil.ROW_TYPE,
+        TableProperties.WRITE_TARGET_FILE_SIZE_BYTES_DEFAULT,
+        FileFormat.PARQUET,
+        null,
+        false);
+
+    taskWriterFactory.initialize(1, 1);
+    return taskWriterFactory.create();
   }
 }

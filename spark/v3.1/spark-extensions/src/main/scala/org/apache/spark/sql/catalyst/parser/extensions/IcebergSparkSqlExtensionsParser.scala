29c9d890b31b2b6d4fda3eb7454d5c84a0dab585
liliwei <liliwei14@huawei.com>
2022-07-26 16:13:51 +0800

    Spark3.1: Spark SQL Extensions for branch & tag

--- spark/v3.1/spark-extensions/src/main/scala/org/apache/spark/sql/catalyst/parser/extensions/IcebergSparkSqlExtensionsParser.scala
+++ spark/v3.1/spark-extensions/src/main/scala/org/apache/spark/sql/catalyst/parser/extensions/IcebergSparkSqlExtensionsParser.scala
@@ -124,7 +124,16 @@ class IcebergSparkSqlExtensionsParser(delegate: ParserInterface) extends ParserI
             normalized.contains("write distributed by") ||
             normalized.contains("write unordered") ||
             normalized.contains("set identifier fields") ||
-            normalized.contains("drop identifier fields")))
+            normalized.contains("drop identifier fields") ||
+            normalized.contains("create tag") ||
+            normalized.contains("replace tag") ||
+            normalized.contains("drop tag") ||
+            normalized.contains("alter tag") ||
+            normalized.contains("create branch") ||
+            normalized.contains("replace branch") ||
+            normalized.contains("drop branch") ||
+            normalized.contains("alter branch") ||
+            normalized.contains("rename branch")))
   }
 
   protected def parse[T](command: String)(toResult: IcebergSqlExtensionsParser => T): T = {
